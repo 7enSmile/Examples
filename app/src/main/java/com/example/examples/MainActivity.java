b7c009ef017443b1e53e02790a4fa4acf6924ba0
7enSmile <68625468+7enSmile@users.noreply.github.com>
2020-09-17 13:34:38 +0700

    Защита

--- app/src/main/java/com/example/examples/MainActivity.java
+++ app/src/main/java/com/example/examples/MainActivity.java
@@ -2,25 +2,41 @@ package com.example.examples;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static android.graphics.Color.rgb;
+
 public class MainActivity extends AppCompatActivity {
+    private EditText objX1;
+    private EditText objX2;
+    private EditText objY1;
+    private EditText objY2;
+    private TextView resText;
+    private Button buttonRes;
+    private int i;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        objX1=(EditText)findViewById(R.id.inputX1);
+        objX2=(EditText)findViewById(R.id.inputX2);
+        objY1=(EditText)findViewById(R.id.inputY1);
+        objY2=(EditText)findViewById(R.id.inputY2);
+        resText=(TextView)findViewById(R.id.textResult);
+        buttonRes=(Button)findViewById(R.id.buttonAccess);
+        i=0;
+
     }
-    public void OnClickButtom(View v){
-        EditText objX1=(EditText)findViewById(R.id.inputX1);
-        EditText objX2=(EditText)findViewById(R.id.inputX2);
-        EditText objY1=(EditText)findViewById(R.id.inputY1);
-        EditText objY2=(EditText)findViewById(R.id.inputY2);
-        TextView resText=(TextView)findViewById(R.id.textResult);
+    public void OnClickRes(View v){
         Double x1=Double.parseDouble(objX1.getText().toString());
         Double x2=Double.parseDouble(objX2.getText().toString());
         Double y1=Double.parseDouble(objY2.getText().toString());
@@ -30,6 +46,19 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this,Double.toString(res),Toast.LENGTH_SHORT).show();
 
 
+    }
+    public void OnClickColor(View v){
+
+        if(i==0)
+            buttonRes.setBackgroundColor(rgb(230,0,0));
+        if(i==1)
+            buttonRes.setBackgroundColor(rgb(255,255,25));
+        if(i==2)
+            buttonRes.setBackgroundColor(rgb(64,255,25));
+        i++;
+        if(i>2) {
+            i = 0;
+        }
     }
 
 }
